d6253a195a6960e18fa8b7ed26e27106672b2120
Abhilash <Abhilash>
2022-01-05 22:00:18 +0530

    sel

---
+++ src/BootstrapDropDown.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BootstrapDropDown {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+
+
+     }
+}
